f50ba3de3942fe28dc5207b8dd9bcf8169443144
Shikha Rathore <71578196+rathoreshikha@users.noreply.github.com>
2021-02-13 11:32:08 +0530

    Add files via upload

---
+++ TEAM/Shikha/Design/tx_parity_gen.v
@@ -0,0 +1,18 @@
+module tx_parity_gen(tx_data_in ,load , parity_bit);
+
+input [7:0] tx_data_in;
+input load;
+output reg parity_bit ;
+
+
+always@(*)
+if(load==0) 
+
+parity_bit=1'b0;
+
+else
+ parity_bit =^(tx_data_in) ;
+
+
+endmodule
+
