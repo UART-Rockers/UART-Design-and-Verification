f69372ecd4fa1e6954730f966fefa18facc48a60
Shikha Rathore <71578196+rathoreshikha@users.noreply.github.com>
2021-04-20 00:17:00 +0530

    input[7:0] rx_data_in ; update this from input rx_data_in ;

--- TEAM/Shikha/Design/parity_checker.v
+++ TEAM/Shikha/Design/parity_checker.v
@@ -1,7 +1,8 @@
 
 module parity_checker(rx_in ,parity_load , rx_data_in ,parity_bit_error);
 
-input rx_in ,parity_load , rx_data_in ; 
+input rx_in ,parity_load ;
+ input[7:0] rx_data_in ; 
 output reg parity_bit_error;
 
 reg temp ;
