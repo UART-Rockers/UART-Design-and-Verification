4de748b6ef6e7e057995035003d333b5145150b4
Shikha Rathore <71578196+rathoreshikha@users.noreply.github.com>
2021-04-25 19:09:49 +0530

    Update parity_checker.v

--- TEAM/Shikha/Design/parity_checker.v
+++ TEAM/Shikha/Design/parity_checker.v
@@ -1,10 +1,10 @@
 
-module parity_checker(rx_in ,parity_load , rx_data_in ,parity_bit_error);
+module parity_checker(rx_in ,parity_load , rx_data_in ,parity_bit_error,parity_out);
 
 input rx_in ,parity_load ;
  input[7:0] rx_data_in ; 
 output reg parity_bit_error;
- output reg [7:0] parityout;
+ output reg [7:0] parity_out;
 
 reg temp ;
 
@@ -14,11 +14,11 @@ always@(parity_load) begin
       temp = ^rx_data_in ;
       if(rx_in==temp) begin
         parity_bit_error <= 0; 
-        parityout<=rx_data_in; 
+        parity_out<=rx_data_in; 
       end
       else begin
         parity_bit_error <= 1;
-        parityout<=rx_data_in;
+        parity_out<=rx_data_in;
       end
     end
   end
