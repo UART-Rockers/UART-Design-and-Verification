9e2324e7b244092848eace9228a6b4bf1ffc57ca
Shikha Rathore <71578196+rathoreshikha@users.noreply.github.com>
2021-04-25 19:07:26 +0530

    Update parity_checker.v
    Add parity_out logic

--- TEAM/Shikha/Design/parity_checker.v
+++ TEAM/Shikha/Design/parity_checker.v
@@ -4,22 +4,24 @@ module parity_checker(rx_in ,parity_load , rx_data_in ,parity_bit_error);
 input rx_in ,parity_load ;
  input[7:0] rx_data_in ; 
 output reg parity_bit_error;
+ output reg [7:0] parityout;
 
 reg temp ;
 
-always@(*)
-begin
-if(parity_load)
-begin
-temp = ^rx_data_in ;
-if(rx_in ==temp)
-parity_bit_error = 0;
- else 
-parity_bit_error = 1 ;
 
-end
-end
-
+always@(parity_load) begin
+    if(parity_load) begin
+      temp = ^rx_data_in ;
+      if(rx_in==temp) begin
+        parity_bit_error <= 0; 
+        parityout<=rx_data_in; 
+      end
+      else begin
+        parity_bit_error <= 1;
+        parityout<=rx_data_in;
+      end
+    end
+  end
 
 
 endmodule 
